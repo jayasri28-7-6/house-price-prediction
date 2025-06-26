6c874a9d109db557e89b0370b08d232f2c40bdde
JAYASRI N <jayasrijayasri067@gmail.com>
2025-06-26 20:22:55 +0530

    Delete output_visualization.html directory

--- output_visualization.html/python predict_price.py
+++
@@ -1,7 +0,0 @@
---- Loading Trained Model from house_price_model.pkl ---
-Model loaded successfully.
-
---- Making Predictions for New Houses ---
-New House 1 (Central, 2500 sqft, 4/3, 2018, Garden): $495,200.75
-New House 2 (West, 1800 sqft, 3/2, 2005): $325,123.45
-New House 3 (East, 1200 sqft, 2/1, 1990): $190,567.89
